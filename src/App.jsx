e67257137bc938d4844c40704a47c0b7027b5fbe
Daniyal-Lodhi <daniyallodhi25@gmail.com>
2023-12-18 20:39:30 +0500

    game part half done

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import React from "react";
 import { BrowserRouter as Router,Routes, Route} from "react-router-dom";
 import Home from './components/Home';
 import Navbar from './components/Navbar';
+import Game from './components/game';
 function App() {
 
   return (
@@ -12,6 +13,7 @@ function App() {
       <Navbar/>
       <Routes>
       <Route path='/' Component={Home} />
+      <Route path='/game' Component={Game} />
       </Routes>
     </Router>
       
